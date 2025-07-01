51bf6cdf22cdb7007ced227b9449ac2d7855fa72
Evillem Walesca <evillemw@gmail.com>
2025-07-01 14:58:03 -0300

    Calculadora React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Visor from "./Componentes/Tela"
+import './App.css'
+function App() {
+
+  return (
+  <div>
+    <h1 className="title">Calculadora</h1>
+    <Visor />
+  </div>
+
+)}
+
+export default App
